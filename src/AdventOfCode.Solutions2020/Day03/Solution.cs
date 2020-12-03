ea453e5bf2949314ca05c015d3ea74faec74cca4
Edvinas Šulcas <e.sulcas@adform.com>
2020-12-03 08:44:06 +0200

    day 3: a little bit harder

---
+++ src/AdventOfCode.Solutions2020/Day03/Solution.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Executor;
+using AdventOfCode.Utilities.Map;
+
+namespace AdventOfCode.Solutions2020.Day03
+{
+    public class Solution : ISolution
+    {
+        public int Day { get; } = 3;
+
+        public string SolveFirstPart(Input input)
+        {
+            var lines = input.GetAsMap();
+
+            var map = new MountainMap(lines);
+
+            var result = CalcTrees(map, new Coordinate(3, 1));
+            
+            return result.ToString();
+        }
+
+        public string SolveSecondPart(Input input)
+        {
+            var lines = input.GetAsMap();
+
+            var map = new MountainMap(lines);
+
+            var result = CalcTrees(map, new Coordinate(1, 1));
+            result *= CalcTrees(map, new Coordinate(3, 1));
+            result *= CalcTrees(map, new Coordinate(5, 1));
+            result *= CalcTrees(map, new Coordinate(7, 1));
+            result *= CalcTrees(map, new Coordinate(1, 2));
+            
+            return result.ToString();
+        }
+
+        private int CalcTrees(MountainMap map, Coordinate velocity)
+        {
+            var position = Coordinate.Zero;
+            var trees = 0;
+
+            while (true)
+            {
+                // Console.WriteLine($"{position}: {map[position]}");
+                
+                if (map[position] == '#')
+                    trees++;
+
+                position += velocity;
+                
+                if (position.Y > map.Height)
+                    break;
+            }
+
+            return trees;
+        }
+        
+        private class MountainMap
+        {
+            private readonly Dictionary<Coordinate, char> _map = new();
+            public int Width { get; }
+            public int Height { get; }
+
+            public MountainMap(Dictionary<(int X, int Y), char> map)
+            {
+                foreach (((int X, int Y) key, char value) in map)
+                {
+                    _map[new Coordinate(key.X, key.Y)] = value;
+                }
+
+                Width = _map.Max(c => c.Key.X);
+                Height = _map.Max(c => c.Key.Y);
+            }
+            
+            public char this[Coordinate index]   
+            {
+                get
+                {
+                    while (index.X > Width)
+                    {
+                        index.X -= Width + 1;
+                    }
+
+                    return _map[index];
+                }
+            }
+        }
+
+    }
+}
--- src/AdventOfCode.Solutions2020/Program.cs
+++ src/AdventOfCode.Solutions2020/Program.cs
@@ -1,31 +1,19 @@
 ﻿using AdventOfCode.Executor;
 using Microsoft.Extensions.Configuration;
 
-namespace AdventOfCode.Solutions2020
-{
-    static class Program
-    {
-        // TODO: command line
-        // TODO: separate downloader
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", true, true)
+    .Build();
 
-        static void Main(string[] args)
-        {
-            IConfiguration config = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", true, true)
-                .Build();
-            
-            var collector = new SolutionCollector();
-            var inputGenerator = new InputFactory(config["year"], config["inputFolder"], config["cookie"]);
+var collector = new SolutionCollector();
+var inputGenerator = new InputFactory(config["year"], config["inputFolder"], config["cookie"]);
 
-            var day = 2;
-            
-            var executor = collector.GetSolutionExecutor(day);
-            
-            // var input = inputGenerator.GetInputFromFile(day, "test1");
-            var input = inputGenerator.GetDefaultInput(day);
+var day = 3;
 
-            executor.ExecuteFirstPart(input);
-            executor.ExecuteSecondPart(input);
-        }
-    }
-}
+var executor = collector.GetSolutionExecutor(day);
+
+// var input = inputGenerator.GetInputFromFile(day, "test1");
+var input = inputGenerator.GetDefaultInput(day);
+
+executor.ExecuteFirstPart(input);
+executor.ExecuteSecondPart(input);
