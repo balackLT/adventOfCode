43cf95c89e7416e27c0277fd6b1f3a14728ee283
Edvinas Šulcas <e.sulcas@adform.com>
2020-12-16 19:47:56 +0200

    2015 day 9: simple pathfinding

--- src/AdventOfCode.Solutions2015/Program.cs
+++ src/AdventOfCode.Solutions2015/Program.cs
@@ -18,7 +18,7 @@ namespace AdventOfCode.Solutions2015
             var collector = new SolutionCollector();
             var inputGenerator = new InputFactory(config["year"], config["inputFolder"], config["cookie"]);
 
-            var day = 8;
+            var day = 9;
             
             var executor = collector.GetSolutionExecutor(day);
             
