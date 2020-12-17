d58f1ac76f5a65100d8fd68a89f147e316371876
Edvinas Šulcas <e.sulcas@adform.com>
2020-12-17 08:55:15 +0200

    day 17: debugging this was hard..

---
+++ src/AdventOfCode.Solutions2020/Day17/Coordinate4D.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+
+namespace AdventOfCode.Solutions2020.Day17
+{
+    public record Coordinate4D (int X, int Y, int Z, int W)
+    {
+        public static Coordinate4D operator +(Coordinate4D left, Coordinate4D right)
+        {
+            return new(left.X + right.X, left.Y + right.Y, left.Z + right.Z, left.W + right.W);
+        }
+        
+        public IEnumerable<Coordinate4D> GetAdjacent()
+        {
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    for (int z = -1; z <= 1; z++)
+                    {
+                        for (int w = -1; w <= 1; w++)
+                        {
+                            if (!(x == 0 && y == 0 && z == 0 && w == 0))
+                                yield return new Coordinate4D(x, y, z, w) + this;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/AdventOfCode.Solutions2020/Day17/Solution.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using AdventOfCode.Executor;
+using AdventOfCode.Utilities.Map;
+
+namespace AdventOfCode.Solutions2020.Day17
+{
+    public partial class Solution : ISolution
+    {
+        public int Day { get; } = 17;
+
+        public string SolveFirstPart(Input input)
+        {
+            var lines = input.GetAsCoordinateMap();
+
+            var map = new Map3D();
+            
+            foreach ((Coordinate coordinate2D, var value) in lines)
+            {
+                var coordinate = new Coordinate3D(coordinate2D.X, coordinate2D.Y, 0);
+                map[coordinate] = (value == '#');
+            }
+            
+            for (int i = 0; i < 6; i++)
+            {
+                map.DoConway3D();
+            }
+
+            var result = map.InternalMap.Count(c => c.Value);
+            
+            return result.ToString();
+        }
+        
+        public string SolveSecondPart(Input input)
+        {
+            var lines = input.GetAsCoordinateMap();
+
+            var map = new Map4D();
+            
+            foreach ((Coordinate coordinate2D, var value) in lines)
+            {
+                var coordinate = new Coordinate4D(coordinate2D.X, coordinate2D.Y, 0, 0);
+                map[coordinate] = (value == '#');
+            }
+            
+            for (int i = 0; i < 6; i++)
+            {
+                map.DoConway4D();
+            }
+
+            var result = map.InternalMap.Count(c => c.Value);
+            
+            return result.ToString();
+        }
+    }
+}
