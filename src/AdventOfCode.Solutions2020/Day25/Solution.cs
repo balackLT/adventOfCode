0a6dd029523ce2d6373d1c6288fc448a65445edb
Edvinas Šulcas <e.sulcas@adform.com>
2020-12-25 11:20:18 +0200

    day 25: aaand it's done

---
+++ src/AdventOfCode.Solutions2020/Day25/Solution.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using AdventOfCode.Executor;
+using Input = AdventOfCode.Executor.Input;
+
+namespace AdventOfCode.Solutions2020.Day25
+{
+    public class Solution : ISolution
+    {
+        public int Day { get; } = 25;
+        
+        private const long Divisor = 20201227;
+
+        public string SolveFirstPart(Input input)
+        {
+            var keys = input.GetLines().Select(long.Parse).ToList();
+
+            Debug.Assert(GuessLoopSize(5764801) == 8);
+            Debug.Assert(Transform(17807724, 8) == 14897079);
+
+            var cardLoopSize = GuessLoopSize(keys[0]);
+            // var doorLoopSize = GuessLoopSize(keys[1]);
+
+            var result = Transform(keys[1], cardLoopSize);
+
+            return result.ToString();
+        }
+        
+        public string SolveSecondPart(Input input)
+        {
+            var lines = input.GetLines();
+
+            return 0.ToString();
+        }
+
+        private int GuessLoopSize(long key)
+        {
+            var loopSize = 0;
+            long value = 1;
+
+            while (true)
+            {
+                loopSize++;
+
+                value = value * 7 % Divisor;
+
+                if (value == key)
+                    return loopSize;
+            }
+        }
+
+        private long Transform(long subject, int loopSize)
+        {
+            long value = 1;
+            
+            for (int i = 0; i < loopSize; i++)
+            {
+                value *= subject;
+                value %= Divisor;
+            }
+
+            return value;
+        }
+    }
+}
--- src/AdventOfCode.Solutions2020/Program.cs
+++ src/AdventOfCode.Solutions2020/Program.cs
@@ -8,7 +8,7 @@ IConfiguration config = new ConfigurationBuilder()
 var collector = new SolutionCollector();
 var inputGenerator = new InputFactory(config["year"], config["inputFolder"], config["cookie"]);
 
-var day = 24;
+var day = 25;
 
 var executor = collector.GetSolutionExecutor(day);
 
