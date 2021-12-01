c5553379a7f2ab6244403a17cb354d24179d30fd
e.sulcas <e.sulcas@adform.com>
2021-12-01 09:11:41 +0200

    2021 first day

---
+++ src/AdventOfCode.Solutions2021/Day01/Solution.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using AdventOfCode.Executor;
+
+namespace AdventOfCode.Solutions2021.Day01
+{
+    public class Solution : ISolution
+    {
+        public int Day { get; } = 1;
+
+        public string SolveFirstPart(Input input)
+        {
+            var lines = input.GetLinesAsInt();
+
+            var prevLine = int.MaxValue;
+            var increaseCount = 0;
+            
+            foreach (var line in lines)
+            {
+                if (line > prevLine)
+                    increaseCount++;
+
+                prevLine = line;
+            }
+
+            return increaseCount.ToString();
+        }
+
+        public string SolveSecondPart(Input input)
+        {
+            var lines = input.GetLinesAsInt();
+
+            var sums = new List<int>();
+
+            for (var i = 2; i < lines.Length; i++)
+            {
+                var slidingSum = lines[i] + lines[i - 1] + lines[i - 2];
+                sums.Add(slidingSum);
+            }
+
+            var prevSum = int.MaxValue;
+            var increaseCount = 0;
+            
+            foreach (var sum in sums)
+            {
+                if (sum > prevSum)
+                    increaseCount++;
+
+                prevSum = sum;
+            }
+            
+            return increaseCount.ToString();
+        }
+
+    }
+}
