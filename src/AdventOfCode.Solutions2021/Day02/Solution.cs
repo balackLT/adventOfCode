d78b3513cf74d2f58ee6f0985f87836071d0070b
e.sulcas <e.sulcas@adform.com>
2021-12-02 08:29:08 +0200

    day 2, still easy

---
+++ src/AdventOfCode.Solutions2021/Day02/Solution.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Executor;
+
+namespace AdventOfCode.Solutions2021.Day02
+{
+    public class Solution : ISolution
+    {
+        public int Day { get; } = 2;
+
+        public string SolveFirstPart(Input input)
+        {
+            var lines = input.GetLines();
+
+            var instructions = lines.Select(l => l.Split());
+
+            var position = 0;
+            var depth = 0;
+            
+            foreach (var instruction in instructions)
+            {
+                var x = int.Parse(instruction[1]);
+                
+                switch (instruction[0])
+                {
+                    case "down":
+                        depth += x;
+                        break;
+                    case "up":
+                        depth -= x;
+                        break;
+                    case "forward":
+                        position += x;
+                        break;
+                }
+            }
+            
+            return (position * depth).ToString();
+        }
+
+        public string SolveSecondPart(Input input)
+        {
+            var lines = input.GetLines();
+
+            var instructions = lines.Select(l => l.Split());
+
+            var position = 0;
+            var depth = 0;
+            var aim = 0;
+            
+            foreach (var instruction in instructions)
+            {
+                var x = int.Parse(instruction[1]);
+                
+                switch (instruction[0])
+                {
+                    case "down":
+                        aim += x;
+                        break;
+                    case "up":
+                        aim -= x;
+                        break;
+                    case "forward":
+                    {
+                        position += x;
+                        depth += x * aim;
+                        break;
+                    }
+                }
+            }
+            
+            return (position * depth).ToString();
+        }
+
+    }
+}
