065a95defee3177443cd1ffa2c728fce4fc98198
e.sulcas <e.sulcas@adform.com>
2023-12-06 08:31:15 +0200

    day 6: finally an actually easy day

---
+++ AdventOfCode.Solutions2023/Day06/Solution.cs
@@ -0,0 +1,53 @@
+﻿using AdventOfCode.Executor;
+
+namespace AdventOfCode.Solutions2023.Day06;
+
+public class Solution : ISolution
+{
+    public int Day { get; } = 6;
+
+    public string SolveFirstPart(Input input)
+    {
+        var lines = input.GetNumbersFromLines();
+
+        var times = lines[0].Select(int.Parse).ToList();
+        var distances = lines[1].Select(int.Parse).ToList();
+
+        var result = 1;
+        for (var t = 0; t < times.Count; t++)
+        {
+            int time = times[t];
+            var variants = 0;
+            
+            for (int heldFor = 1; heldFor < time; heldFor++)
+            {
+                var raceResult = (time - heldFor) * heldFor;
+                if (raceResult > distances[t])
+                    variants++;
+            }
+            
+            result *= variants;
+        }
+
+        return result.ToString();
+    }
+
+    public string SolveSecondPart(Input input)
+    {
+        var lines = input.GetNumbersFromLines();
+
+        var time = long.Parse(string.Concat(lines[0]));
+        var distance = long.Parse(string.Concat(lines[1]));
+        
+        var variants = 0;
+
+        for (int heldFor = 1; heldFor < time; heldFor++)
+        {
+            var raceResult = (time - heldFor) * heldFor;
+            if (raceResult > distance)
+                variants++;
+        }
+
+        return variants.ToString();
+    }
+}
--- AdventOfCode.Solutions2023/Program.cs
+++ AdventOfCode.Solutions2023/Program.cs
@@ -7,7 +7,7 @@ var config = new ConfigurationBuilder()
 var collector = new SolutionCollector();
 var inputGenerator = new InputFactory(config["year"], config["inputFolder"], config["cookie"]);
 
-const int day = 5;
+const int day = 6;
 
 var executor = collector.GetSolutionExecutor(day);
 
