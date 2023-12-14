581208260838510a4c0a77c0cfb4d678d4ebac9b
e.sulcas <e.sulcas@adform.com>
2023-12-14 15:03:09 +0200

    day 14 part 2: over engineered but works

--- AdventOfCode.Solutions2023/Day14/Solution.cs
+++ AdventOfCode.Solutions2023/Day14/Solution.cs
@@ -13,12 +13,7 @@ public class Solution : ISolution
         
         MoveAllRocks(map, Coordinate.South);
         
-        var result = 0L;
-        var maxY = map.MaxY();
-        foreach (var rock in map.Where(i => i.Value == 'O'))
-        {
-            result += maxY - rock.Key.Y + 1;
-        }
+        long result = CalculateWeight(map);
 
         return result;
     }
@@ -26,9 +21,28 @@ public class Solution : ISolution
     private static void MoveAllRocks(Dictionary<Coordinate, char> map, Coordinate direction)
     {
         var movableRocks = new Queue<Coordinate>();
-        foreach (var item in map.Where(i => i.Value == 'O'))
+
+        IEnumerable<KeyValuePair<Coordinate, char>> allRocks = map.Where(i => i.Value == 'O');
+        IEnumerable<KeyValuePair<Coordinate, char>> orderedRocks = new List<KeyValuePair<Coordinate, char>>();
+        if (direction == Coordinate.South)
         {
-            
+            orderedRocks = allRocks;
+        }
+        else if (direction == Coordinate.West)
+        {
+            orderedRocks = allRocks.OrderBy(i => i.Key.X);
+        }
+        else if (direction == Coordinate.North)
+        {
+            orderedRocks = allRocks.OrderByDescending(i => i.Key.Y);
+        }
+        else if (direction == Coordinate.East)
+        {
+            orderedRocks = allRocks.OrderByDescending(i => i.Key.X);
+        }
+        
+        foreach (var item in orderedRocks)
+        {
             movableRocks.Enqueue(item.Key);
         }
 
@@ -58,6 +72,71 @@ public class Solution : ISolution
     {
         var map = input.GetAsCoordinateMap();
 
-        return 0;
+        var max = 1000000000;
+        var previousMaps = new List<string>();
+        var cycleSize = 0;
+        for (long i = 0; i < max; i++)
+        {
+            var hash = map.Aggregate("", (current, c) => current + c.Value);
+            if (cycleSize == 0 && previousMaps.Contains(hash))
+            {
+                // cycle detected, only boring stuff further on
+                cycleSize = previousMaps.Count - previousMaps.LastIndexOf(hash);
+                // skip boring cycles
+                while (i < max - cycleSize)
+                {
+                    i += cycleSize;
+                }
+            }
+            previousMaps.Add(hash);
+
+            map = SpinCycle(map);
+        }
+        
+        map.Print();
+        
+        long result = CalculateWeight(map);
+
+        return result;
+    }
+
+    private readonly Dictionary<string, Dictionary<Coordinate, char>> _cache = new();
+
+    private Dictionary<Coordinate, char> SpinCycle(Dictionary<Coordinate, char> map)
+    {
+        var hash = map.Aggregate("", (current, c) => current + c.Value);
+        
+        if (_cache.TryGetValue(hash, out var cachedMap))
+        {
+            return cachedMap;
+        }
+
+        var newMap = SpinCycleCore(map);
+        _cache[hash] = newMap;
+        return newMap;
+    }
+    
+    private static Dictionary<Coordinate, char> SpinCycleCore(Dictionary<Coordinate, char> map)
+    {
+        var newMap = new Dictionary<Coordinate, char>(map);
+        
+        MoveAllRocks(newMap, Coordinate.South);
+        MoveAllRocks(newMap, Coordinate.West);
+        MoveAllRocks(newMap, Coordinate.North);
+        MoveAllRocks(newMap, Coordinate.East);
+
+        return newMap;
+    }
+
+    private static long CalculateWeight(Dictionary<Coordinate, char> map)
+    {
+        var result = 0L;
+        var maxY = map.MaxY();
+        foreach (var rock in map.Where(i => i.Value == 'O'))
+        {
+            result += maxY - rock.Key.Y + 1;
+        }
+
+        return result;
     }
 }
